3f9740b530011e555f5cb24f2c45622feea84b6b
Alex Yagodkin <adamsHillzzz@gmail.com>
2024-11-13 14:20:11 +0300

    feat: add ooze waves

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,1 +1,29 @@
 import '../css/style.css';
+
+// class Ooze {
+// 	private canvas: HTMLCanvasElement;
+// 	private context: CanvasRenderingContext2D | null;
+// 	constructor() {
+// 		let canvas = document.getElementById('ooze') as HTMLCanvasElement;
+// 		let context = canvas?.getContext('2d');
+
+// 		this.canvas = canvas;
+// 		this.context = context;
+
+// 		this.draw();
+// 	}
+
+// 	private draw() {
+// 		if (this.context) {
+// 			this.context.fillStyle = 'rgb(200, 0, 0)';
+// 			this.context.fillRect(10, 10, 30, 50);
+
+// 			this.context.fillStyle = 'rgba(0, 0, 200, 0.5)';
+// 			this.context.fillRect(30, 30, 55, 50);
+// 		}
+// 	}
+
+// 	private clean() {}
+// }
+
+// const ooze = new Ooze();
