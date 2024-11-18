1a4be6b6522eaa5af2c43f56059392e8bb0dd741
Alex Yagodkin <adamsHillzzz@gmail.com>
2024-11-18 17:36:37 +0300

    feat: change colors

--- src/scripts/core/defaultValues.ts
+++ src/scripts/core/defaultValues.ts
@@ -1,11 +1,11 @@
 export const defaultValues = {
-	colors: ['rgba(255, 255, 255, 1)', 'rgba(255, 255, 255, 0.6)', 'rgba(255, 255, 255, 0.4)'],
+	colors: ['rgba(224, 124, 155, 0.5)', 'rgba(0, 127, 55, 0.3)', 'rgba(255, 102, 1, 0.4)'],
 	pointsPerWave: 30,
 	numWaves: 3,
 	waveSpeed: 0.015,
 	offsetsWave: [
 		{
-			waveIndex: 1,
+			waveIndex: 0,
 			offsetValue: 180,
 		},
 		{
